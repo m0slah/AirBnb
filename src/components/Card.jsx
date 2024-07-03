7aa077bf62ae07fcf8fb83da0f7281b0059ee09f
m0slah <muhammedslahqadir@gmail.com>
2024-07-03 11:38:28 +0300

    A11Y Audit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,14 @@ import star from "../assets/Images/star.png";
 export default function Card() {
   return (
     <div className="card">
-      <img src={swim} className="card--image" />
+      <img src={swim} className="card--image" alt="Main card image." />
       <div className="card--stats">
-        <img src={star} className="card--star" />
+        <img src={star} className="card--star" alt="Star icon." />
         <span>5.0</span>
         <span className="gray">(6) • </span>
         <span className="gray">USA</span>
       </div>
-      <p>Life Lessons with Katie Zaferes</p>
+      <h2>Life Lessons with Katie Zaferes</h2>
       <p>
         <span className="bold">From $136</span> / person
       </p>
