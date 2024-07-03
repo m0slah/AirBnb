e4bf3ae9fd0ef1a2e51f30df074ad4cb6a4df600
m0slah <muhammedslahqadir@gmail.com>
2024-07-03 12:03:06 +0300

    JS inside JSX

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,17 @@ Check the Figma file for the design specifics.
 */
 
 export default function App() {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Card />
-    </div>
-  );
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+
+  return <h1>Good {timeOfDay}!</h1>;
 }
