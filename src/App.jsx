ba6b172cb8265fd79647027019a0f57391914f29
m0slah <muhammedslahqadir@gmail.com>
2024-07-03 09:43:12 +0300

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar";
 
-function App() {
+export default function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <Navbar />
+    </div>
+  );
 }
-
-export default App
