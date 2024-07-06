a82f1fd4824fc3d62f0483e7ac5c9e93a2203e7b
m0slah <muhammedslahqadir@gmail.com>
2024-07-06 14:49:27 +0300

    key prop

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import data from "./data.js";
+import data from "./data";
 
 export default function App() {
-  const cardData = data.map((item) => (
-    <div key={item.id}>
-      <Navbar />
+  const cards = data.map((item) => {
+    return (
       <Card
-        coverImg={item.coverImg} // Assuming `swim` is your imported image source
-        rating={item.rating} // Example: dynamically use rating from data
-        reviewCount={item.reviewCount} // Example: dynamically use review count from data
-        country={item.country} // Example: dynamically use country from data
-        title={item.title} // Example: dynamically use title from data
-        price={item.price} // Example: dynamically use price from data
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
       />
-    </div>
-  ));
+    );
+  });
 
-  return <div>{cardData}</div>;
+  return (
+    <div>
+      <Navbar />
+      {/* <Hero /> */}
+      <section className="cards-list">{cards}</section>
+    </div>
+  );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,19 @@
-import star from "../assets/Images/star.png";
+import React from "react";
+import starImage from "../assets/Images/star.png";
 
-export default function Card({
-  coverImg,
-  rating,
-  reviewCount,
-  country,
-  title,
-  price,
-}) {
+export default function Card(props) {
   return (
     <div className="card">
-      <img
-        src={`../images/${coverImg}`}
-        className="card--image"
-        alt="Main card image."
-      />
+      <img src={props.img} className="card--image" />
       <div className="card--stats">
-        <img src={star} className="card--star" alt="Star icon." />
-        <span>{rating}</span>
-        <span className="gray">{reviewCount} • </span>
-        <span className="gray">({country})</span>
+        <img src={starImage} className="card--star" />
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
       </div>
-      <h2>{title}</h2>
-      <p>
-        <span className="bold">{price}</span> / person
+      <p className="card--title">{props.title}</p>
+      <p className="card--price">
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   );
